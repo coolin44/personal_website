83c26073772125916c06d0823f1146558bacbd44
William Hubert <wcolinhubert94@gmail.com>
2019-02-27 20:22:40 -0600

    hopefully it works now

--- src/pages/contactme.js
+++ src/pages/contactme.js
@@ -5,16 +5,20 @@ const ContactMePage = () => (
   <Layout>
     <div>
     <h1>Contact Me</h1>
-
-    <form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-    >
-        <input name="name" placeholder="Your Name" type="text"/>
-        <button type="submit">Send</button>
-    </form>
+    <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 
 
     </div>
